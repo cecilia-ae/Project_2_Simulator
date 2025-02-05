551c5a56982d20059065335a2f1065ff5246d14a
hermannml <MLH280@pitt.edu>
2025-02-04 21:22:58 -0500

    Add files via upload

--- Bus.py
+++ Bus.py
@@ -1,10 +1,26 @@
+# Group 8 - Project 2
+# ECE 2774
+# Milestone 1
+
 class Bus:
 
     # Class variable to keep track of the number of Bus instances
-    instance_count = 0
+    bus_count = 0
 
     def __init__(self, name: str, base_kv: float):
         self.name = name
         self.base_kv = base_kv
-        Bus.instance_count += 1
-        self.index = Bus.instance_count
+        Bus.bus_count += 1
+        self.index = Bus.bus_count
+
+# Validation
+if __name__ == "__main__":
+
+    bus1 = Bus("Bus 1", 20)
+    bus2 = Bus("Bus 2", 230)
+    print("Bus 1:", bus1.name, bus1.base_kv, "Index:", bus1.index)
+    print("Bus 2:", bus2.name, bus2.base_kv, "Index:", bus2.index)
+    print("Total Buses:", Bus.bus_count)
+
+
+
--- Transformer.py
+++ Transformer.py
@@ -1,5 +1,8 @@
+# Group 8 - Project 2
+# ECE 2774
+# Milestone 1
 import numpy as np
-
+import Bus as Bus
 class Transformer:
 
     def __init__(self, name: str, bus1: str, bus2: str, power_rating: float,
@@ -12,26 +15,39 @@ class Transformer:
         self.x_over_r_ratio = x_over_r_ratio
 
         # calculate the impedance and admittance values
-        self.impedance = self.calc_impedance()
-        self.admittance = self.calc_admittance()
+        self.zt = self.calculate_impedance()
+        self.yt = self.calculate_admittance()
+        self.yprim = self.y_primitive_matrix()
 
-    def calc_impedance(self):
-        # calculate the impedance of the transformer
-        base_impedance = 100  # assume 100 MVA system base
+    def calculate_impedance(self):
+
+        base_impedance = 100  # Assume 100 MVA system base
         z_base = base_impedance / self.power_rating
         r = z_base * self.impedance_percent / (100 * np.sqrt(1 + self.x_over_r_ratio ** 2))
         x = r * self.x_over_r_ratio
         return complex(r, x)
 
-    def calc_admittance(self):
-        if self.impedance == 0:
+    def calculate_admittance(self):
+
+        if self.zt == 0:
             return complex(0, 0)
-        return 1 / self.impedance
+        return 1 / self.zt
 
-    def calc_y_prim(self):
-        y_prim = np.array([
-            [self.admittance, -self.admittance],
-            [-self.admittance, self.admittance]
+    def y_primitive_matrix(self):
+
+        yprim = np.array([
+            [self.yt, -self.yt],
+            [-self.yt, self.yt]
         ])
-        return y_prim
+        return yprim
 
+# Validation
+if __name__ == "__main__":
+    transformer1 = Transformer("T1", "bus1", "bus2", 125,8.5,10)
+    print("Transformer Name:", transformer1.name)
+    print("Connected Buses:", transformer1.bus1, "<-->", transformer1.bus2)
+    print("Power Rating:", transformer1.power_rating, "MVA")
+    print("Impedance (zt):", transformer1.zt)
+    print("Admittance (yt):", transformer1.yt)
+    print("Yprim matrix:")
+    print(transformer1.yprim)
