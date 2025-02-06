76774c96d984f92e04418c30ae89af94fa100195
cecilia-ae <cae68@pitt.edu>
2025-02-06 09:02:23 -0500

    Add files via upload

---
+++ Global.py
@@ -0,0 +1,8 @@
+# Group 8 - Project 2
+# ECE 2774
+# Global Variables
+
+class Global:
+    Sbase = 100  # [MVA] # should this be assumed or no
+    f = 60  # Frequency in Hz
+    ε_0 = 8.854 * 10 ** -12  # permittivity of free space (F/m)
