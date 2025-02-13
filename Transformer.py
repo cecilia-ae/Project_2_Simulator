a781df76dc92350cf812f3249c977db08089fe7b
hermannml <MLH280@pitt.edu>
2025-02-13 08:52:57 -0500

    Add files via upload

--- Transformer.py
+++ Transformer.py
@@ -2,10 +2,12 @@
 # ECE 2774
 # Milestone 1
 import numpy as np
+import pandas as pd
+from Bus import Bus
 
 class Transformer:
 
-    def __init__(self, name: str, bus1: str, bus2: str, power_rating: float,
+    def __init__(self, name: str, bus1: Bus, bus2: Bus, power_rating: float,
                  impedance_percent: float, x_over_r_ratio: float):
         self.name = name
         self.bus1 = bus1
@@ -20,7 +22,7 @@ class Transformer:
         self.yprim = self.calc_yprim()
 
     def calc_impedance(self):
-        
+
         base_impedance = 100  # Assume 100 MVA system base
         z_base = base_impedance / self.power_rating
 
@@ -42,11 +44,18 @@ class Transformer:
             [self.Yseries, -self.Yseries],
             [-self.Yseries, self.Yseries]
         ])
-        return yprim
+        yprim_df = pd.DataFrame(yprim, index=[self.bus1.name, self.bus2.name], columns=[self.bus1.name, self.bus2.name])
+
+        return yprim_df
 
 # Validation
 if __name__ == "__main__":
-    transformer1 = Transformer("T1", "bus1", "bus2", 125, 8.5, 10)
+    # define bus1 and bus2 before
+    bus1 = Bus("Bus1", 20)
+    bus2 = Bus("Bus2",230)
+
+    transformer1 = Transformer("T1", bus1, bus2, 125, 8.5, 10)
+
     print("Transformer Name:", transformer1.name)
     print("Connected Buses:", transformer1.bus1, "<-->", transformer1.bus2)
     print("Power Rating:", transformer1.power_rating, "MVA")
