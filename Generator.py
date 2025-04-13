e1745155fc3c0f1e0d706ed6b91de4a7e24f5fda
cecilia-ae <cae68@pitt.edu>
2025-04-13 18:34:42 -0400

    Add files via upload

--- Generator.py
+++ Generator.py
@@ -3,11 +3,38 @@
 # Milestone 5
 
 from Bus import Bus
+import numpy as np
 
 class Generator:
 
-    def __init__(self, name: str, bus: Bus, voltage_setpoint: float, mw_setpoint: float):
+    def __init__(self,name: str, bus: Bus, voltage_setpoint: float, mw_setpoint: float, grounding_impedance: complex = 0 + 0j, is_grounded: bool = True):
         self.name = name
         self.bus = bus
         self.voltage_setpoint = voltage_setpoint
         self.mw_setpoint = mw_setpoint
+
+        # sequence reactances
+        self.x1 = 0.12 # positive-sequence subtransient reactance
+        self.x2 = 0.14 # negative-sequence subtransient reactance
+        self.x0 = 0.05 # zero-sequence subtransient reactance
+
+        # grounding configuration
+        self.grounding_impedance = grounding_impedance #default of zero which represents a solid ground
+        self.is_grounded = is_grounded
+
+    def y_prim_negative_sequence(self) -> complex:
+            # primitive admittance (Y = 1 / jX2) for the negative-sequence network.
+
+            Yprim2 = 1 / (1j * self.x2)
+
+            return Yprim2
+
+    def y_prim_zero_sequence(self) -> complex:
+        # primitive admittance (Y = 1 / (jX0 + 3*Zn)) for the zero-sequence network.
+        # if generator is ungrounded, return 0
+
+        Yprim0 = 1 / (1j * self.x0 + 3 * self.grounding_impedance)
+
+        if not self.is_grounded:
+            return 0 + 0j
+        return Yprim0
